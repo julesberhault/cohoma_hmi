f8a26447d7475d77be4cb4fcf11f35453a1af0d4
Pafnouti <corentinlemoine22@gmail.com>
2020-03-18 15:20:25 +0100

    Path now displayed

--- public/js/map.js
+++ public/js/map.js
@@ -28,47 +28,52 @@ var myMovingMarker = new L.marker([48.370954, -4.480665]).addTo(map);
 // WP List machinery
 var el = document.getElementById('wayPointsList');
 
+var swapArrayElements = function (arr, indexA, indexB) {
+    var temp = arr[indexA];
+    arr[indexA] = arr[indexB];
+    arr[indexB] = temp;
+};
+
+var wayPointsList = []
+
 var sortable = Sortable.create(el, {
     onEnd: function (evt) {
         var itemEl = evt.item;  // dragged HTMLElement
-        console.log(itemEl.id, evt.oldIndex, evt.newIndex)
+        console.log(itemEl.id, evt.oldIndex, evt.newIndex);
+        swapArrayElements(wayPointsList, evt.oldIndex, evt.newIndex);
+        updatePath();
     }
 });
 
-var wayPointsList = [
-    {
-        latlong: [48.370954, -4.480665],
-        id: "id1",
-        currid: 5
-    },
-    {
-        latlong: [48.380, -4.480665],
-        id: "id2",
-        currid: 5
-    },
-    {
-        latlong: [48.370954, -4.4850],
-        id: "id3",
-        currid: 5
-    },
-    {
-        latlong: [48.370954, -4.475],
-        id: "id4",
-        currid: 5
-    }
-];
+
+
+var updatePath = function () {
+    var latlongs = [];
+    wayPointsList.forEach(element => {
+        latlongs.push(element.latlong);
+    });
+    polyline.setLatLngs(latlongs);
+}
 
 var currID = 5;
 
 var deleteWPf = function (event) {
     var id = $(this).closest("li").attr("id");
+    var i = 0;
+    var k = 0;
     wayPointsList.forEach(element => {
-        if(id==element.id){
+        if (id == element.id) {
             map.removeLayer(element.marker);
+            k = i;
         }
+        i++;
     });
+    wayPointsList.splice(k, 1);
     $(this).closest("li").remove();
+    updatePath();
 };
+
+
 
 $(".deleteWP").click(deleteWPf);
 
@@ -94,11 +99,19 @@ $(".submitWP").click(function (event) {
         $(this).closest("li").remove();
     });
 });
+
 var posShow = function (position) {
     return (Math.trunc(10000 * position.lat) / 10000 + ' : ' + Math.trunc(10000 * position.lng) / 10000)
 }
+
 var updateWPList = function (wps) {
     //wps must contain an id and a latlong array at the bare minimum
+    wayPointsList.forEach(element => {
+        if(element.marker){
+            map.removeLayer(element.marker);
+        }
+    });
+
     wayPointsList = [];
     wps.forEach(wp => {
         var marker = new L.marker(wp.latlong, {
@@ -113,6 +126,7 @@ var updateWPList = function (wps) {
 
             document.getElementById(wp.id).childNodes[0].nodeValue = posShow(position);
 
+            updatePath();
         });
         wp.marker = marker;
         map.addLayer(marker);
@@ -124,9 +138,38 @@ var updateWPList = function (wps) {
         var position = element.marker.getLatLng();
         $("#wayPointsList").append('<li class="list-group-item wpItem" id="' + element.id + '">' + posShow(position) + '<button class="btn btn-danger deleteWP" type="button">-</button></li>');
     });
-    
+
     $(".deleteWP").click(deleteWPf);
 }
+
+wayPointsList = [
+    {
+        latlong: [48.370954, -4.480665],
+        id: "id1",
+        currid: 5
+    },
+    {
+        latlong: [48.380, -4.480665],
+        id: "id2",
+        currid: 5
+    },
+    {
+        latlong: [48.370954, -4.4850],
+        id: "id3",
+        currid: 5
+    },
+    {
+        latlong: [48.370954, -4.475],
+        id: "id4",
+        currid: 5
+    }
+];
+
+updateWPList(wayPointsList);
+
+var polyline = L.polyline([], { color: 'red' }).addTo(map);
+
+updatePath();
 
 /*
 m.slideTo([48.864433, 2.371324], {
